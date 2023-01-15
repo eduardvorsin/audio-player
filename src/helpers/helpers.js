c0cce7ea06e017bc58f1bbf7eea84b58698578ff
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-01-15 20:07:38 +0600

    added checks for infinite and for NaN

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,4 +1,12 @@
 export const formatTime = (secs) => {
+	if (Number.isNaN(+secs)) {
+		throw new Error('the passed value must be a number');
+	}
+
+	if (!Number.isFinite(secs)) {
+		throw new Error('the passed value must be a finite number');
+	}
+
 	const minutes = Math.floor(secs / 60);
 	const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
 	const seconds = Math.floor(secs % 60);
