9a2cc1633c581dab1d53cd2a326d86765b259cb9
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-01-15 19:45:02 +0600

    [tests] added tests for TrackInfo Component

---
+++ src/components/AudioPlayer/TrackInfo/TrackInfo.test.js
@@ -0,0 +1,32 @@
+import { render, screen } from "@testing-library/react";
+import { TrackInfo } from "./TrackInfo";
+
+describe('Track Info component tests', () => {
+  test('renders correctly', () => {
+    render(<TrackInfo
+      trackName='track'
+      trackArtist='artist'
+    />);
+
+    expect(screen.getByText('track')).toBeInTheDocument();
+    expect(screen.getByText('artist')).toBeInTheDocument();
+  });
+
+  test('renders without passed props', () => {
+    render(<TrackInfo />);
+
+    expect(screen.getByText('trackName')).toBeInTheDocument();
+    expect(screen.getByText('trackArtist')).toBeInTheDocument();
+  });
+
+  test('className is assigned correctly', () => {
+    render(
+      <TrackInfo
+        className='test'
+        testId='track-info'
+      />
+    );
+
+    expect(screen.getByTestId('track-info')).toHaveClass('test');
+  });
+});
