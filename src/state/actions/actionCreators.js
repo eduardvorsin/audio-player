a4117ebc4e9c8e752c0ce3b1401933ebb002f87c
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-01-16 19:24:04 +0600

    added actionCreator functions

--- src/state/actions/actionCreators.js
+++ src/state/actions/actionCreators.js
@@ -8,3 +8,55 @@ export const PLAY = 'play';
 export const PAUSE = 'pause';
 export const LOOP = 'loop';
 export const UNLOOP = 'unloop';
+
+export const changeDurationAC = (value) => {
+	return {
+		type: CHANGE_DURATION,
+		payload: value,
+	}
+};
+
+export const changeCurrentTimeAC = (value) => {
+	return {
+		type: CHANGE_CURRENT_TIME,
+		payload: value,
+	}
+};
+
+export const changeVolumeAC = (value) => {
+	return {
+		type: CHANGE_VOLUME,
+		payload: value,
+	}
+};
+
+export const changePlaybackRateAC = (value) => {
+	return {
+		type: CHANGE_PLAYBACKRATE,
+		payload: value,
+	}
+};
+
+export const playAudioAC = () => ({
+	type: PLAY,
+});
+
+export const pauseAudioAC = () => ({
+	type: PAUSE,
+});
+
+export const muteAudioAC = () => ({
+	type: MUTE,
+});
+
+export const unmuteAudioAC = () => ({
+	type: UNMUTE,
+});
+
+export const loopAudioAC = () => ({
+	type: LOOP,
+});
+
+export const unloopAudioAC = () => ({
+	type: UNLOOP,
+});
