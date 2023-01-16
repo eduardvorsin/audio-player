0887fd9a56413b2e2427b29c644e307371671548
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-01-16 14:36:40 +0600

    moved track data to another file

--- src/App.js
+++ src/App.js
@@ -72,46 +72,9 @@ select {
 }
 `;
 
-const tracks = [
-  {
-    id: 1,
-    src: './assets/audio/guitar-electro-sport-trailer-115571.mp3',
-    trackName: 'Guitar Electro Sport Trailer',
-    trackArtist: 'Gvidon',
-    trackImage: {
-      src: './assets/images/electro-guitar.jpg',
-      alt: 'electric guitar on neon background',
-    },
-    sources: [
-      './assets/audio/guitar-electro-sport-trailer-115571.ogg',
-      './assets/audio/guitar-electro-sport-trailer-115571.wav'
-    ],
-  },
-  {
-    id: 2,
-    src: './assets/audio/milk-shake-116330.mp3',
-    trackName: 'Milk Shake',
-    trackArtist: 'Coma-Media',
-    trackImage: {
-      src: './assets/images/milk-shake.jpg',
-      alt: 'milk cocktail on a blue background',
-    },
-    sources: [],
-  },
-  {
-    id: 3,
-    src: './assets/audio/goldn-116392.mp3',
-    trackName: 'Goldn',
-    trackArtist: 'prazkhanal',
-    trackImage: {
-      src: './assets/images/goldn.jpg',
-      alt: 'golden leaves',
-    },
-    sources: [],
-  },
-];
-
-const App = () => {
+const App = ({
+  tracks
+}) => {
   const [trackIndex, setTrackIndex] = useState(0);
 
   const handleClickPrevious = () => {
