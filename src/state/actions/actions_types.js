7688fd945d7e178e33362cc26352a78426b16e21
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-01-16 18:42:29 +0600

    removed unused action

--- src/state/actions/actions_types.js
+++ src/state/actions/actions_types.js
@@ -2,7 +2,6 @@ export const CHANGE_DURATION = 'change-duration';
 export const CHANGE_CURRENT_TIME = 'change-current-time';
 export const CHANGE_VOLUME = 'change-volume';
 export const CHANGE_PLAYBACKRATE = 'change-playback-rate';
-export const TOGGLE_LOOPING = 'toggle-looping';
 export const MUTE = 'mute';
 export const UNMUTE = 'unmute';
 export const PLAY = 'play';
