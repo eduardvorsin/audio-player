2aa62ee661999fb264ab94aa42e0acd7bf878329
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-01-18 17:56:32 +0600

    [tests] added snapshot tests for AudioControls component

--- src/components/AudioPlayer/AudioControls/AudioControls.test.js
+++ src/components/AudioPlayer/AudioControls/AudioControls.test.js
@@ -34,4 +34,110 @@ describe('AudioControls component tests', () => {
 
     expect(screen.queryByRole('button', { name: 'mute' })).not.toBeInTheDocument();
   });
+
+  test('snapshot with default prop values', () => {
+    render(
+      <AudioControls />
+    );
+
+    expect(screen.getByTestId('audio-controls')).toMatchSnapshot();
+  });
+
+  test('snapshot with isPlayed & isMuted props', () => {
+    render(
+      <AudioControls
+        isPlayed
+        isMuted
+      />
+    );
+
+    expect(screen.getByTestId('audio-controls')).toMatchSnapshot();
+  });
+
+  test('snapshot with a modified volume prop', () => {
+    render(
+      <AudioControls
+        volume={33}
+      />
+    );
+
+    expect(screen.getByTestId('audio-controls')).toMatchSnapshot();
+  });
+
+  test('snapshot with showDownloadControl & downloadLink props', () => {
+    render(
+      <AudioControls
+        showDownloadControl
+        downloadLink='test.mp3'
+      />
+    );
+
+    expect(screen.getByTestId('audio-controls')).toMatchSnapshot();
+  });
+
+  test('snapshot with showPlaybackRateControl prop', () => {
+    render(
+      <AudioControls
+        showPlaybackRateControl
+      />
+    );
+
+    expect(screen.getByTestId('audio-controls')).toMatchSnapshot();
+  });
+
+  test('snapshot with showPlaybackRateControl & playbackRate props', () => {
+    render(
+      <AudioControls
+        showPlaybackRateControl
+        playbackRate={1.25}
+      />
+    );
+
+    expect(screen.getByTestId('audio-controls')).toMatchSnapshot();
+  });
+
+  test('snapshot with showLoopControl prop', () => {
+    render(
+      <AudioControls
+        showLoopControl
+      />
+    );
+
+    expect(screen.getByTestId('audio-controls')).toMatchSnapshot();
+  });
+
+  test('snapshot with showLoopControl & isLooped props', () => {
+    render(
+      <AudioControls
+        showLoopControl
+        isLooped
+      />
+    );
+
+    expect(screen.getByTestId('audio-controls')).toMatchSnapshot();
+  });
+
+  test('snapshot with showNextAndPreviousControls prop', () => {
+    render(
+      <AudioControls
+        showNextAndPreviousControls
+      />
+    );
+
+    expect(screen.getByTestId('audio-controls')).toMatchSnapshot();
+  });
+
+  test('snapshot with all the props affecting the UI', () => {
+    render(
+      <AudioControls
+        showDownloadControl
+        showPlaybackRateControl
+        showLoopControl
+        showNextAndPreviousControls
+        className='test'
+      />
+    );
+
+    expect(screen.getByTestId('audio-controls')).toMatchSnapshot();
+  });
 });
